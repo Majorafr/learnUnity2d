e24f9d548412a93c52c0a51e81384afa3335e63b
Majorafr <corentin.jean.gourmel@gmail.com>
2019-05-27 16:32:51 +0200

    setup

---
+++ UnityProject/Assets/player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float playerSpeed;
+    private Rigidbody2D MyrigidBody;
+    private bool flipSprite;
+    private SpriteRenderer spritePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyrigidBody = GetComponent<Rigidbody2D>();
+        flipSprite = false;
+        spritePlayer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        
+
+        HandleMovement(horizontal);
+        if (horizontal > 0)
+        {
+            spritePlayer.flipX = true;
+        }
+        else spritePlayer.flipX = false;
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        MyrigidBody.velocity = new Vector2(horizontal * playerSpeed, MyrigidBody.velocity.y);
+    }
+    
+
+
+}   
---
+++ UnityProject/Assets/snapCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snapCamera : MonoBehaviour
+{
+    /*public GameObject sceneSize;
+    public Camera mainCamera;
+    public float sizeCamera;
+    private float getSceneSize;*/
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*getSceneSize = sceneSize.GetComponent<Transform>().localScale.x;
+        mainCamera.orthographic = true;
+        mainCamera.orthographicSize = sizeCamera;
+        Debug.Log(getSceneSize); */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
